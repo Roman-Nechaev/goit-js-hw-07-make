d0d4346690ce8bcb54607a2f642be0645cbfcabe
Roman-Nechaev <nrchaevroman@gmail.com>
2022-01-19 01:30:38 +0200

    1step

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,25 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryListRef = document.querySelector('.gallery');
+
+const galleryCard = ({ preview, original, description }) => {
+    const wrapperGallery = document.createElement('div');
+    wrapperGallery.innerHTML = `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`;
+    return wrapperGallery;
+};
+
+const galleryCards = galleryItems.map(galleryItem => galleryCard(galleryItem));
+galleryListRef.append(...galleryCards);
